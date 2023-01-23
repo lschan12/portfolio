0ca237698b97d225daf98c8adacd7966238c76b3
Lawrence Chan <lawrence.c@hotmail.co.uk>
2023-01-23 11:20:58 -0800

    modify contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ const Contact = () => {
     <div id="contact">
       <h1>Get in touch</h1>
       <p>
-        Please connect with me about anything tech! I am open to any questions or if you simple want to chat and get to know me!
+        Please connect with me about anything tech! I am open to any questions or if you simply want to chat and get to know me!
       </p>
       <a href="mailto:lawrence.c@hotmail.co.uk">Email me!</a>
     </div>
